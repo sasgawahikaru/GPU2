ceaef5b31b7287482a51c3ddb4498f7332cf0928
sasagawahikaru <k021g1383@g.neec.ac.jp>
2022-12-09 11:53:32 +0900

    4_3 多分完成

--- Resources/shaders/OBJPixelShader.hlsl
+++ Resources/shaders/OBJPixelShader.hlsl
@@ -11,7 +11,7 @@ float4 main(VSOutput input) : SV_TARGET
 //	shade_color = m_ambient; // アンビエント項
 //	shade_color += m_diffuse * light_diffuse;	// ディフューズ項
 	float4 texcolor = tex.Sample(smp, input.uv);
-	return input.color;
+	return input.color * texcolor;
 //	return float4(1, 1, 1, 1);
 //	return float4(texcolor.rgb * shade_color, texcolor.a * m_alpha);
 }
--- Resources/shaders/OBJVertexShader.hlsl
+++ Resources/shaders/OBJVertexShader.hlsl
@@ -2,6 +2,7 @@
 
 VSOutput main(float4 pos : POSITION, float3 normal : NORMAL, float2 uv : TEXCOORD)
 {
+
 	float3 lightdir = float3(1, -1, 1);
 	lightdir = normalize(lightdir);
 
@@ -9,9 +10,21 @@ VSOutput main(float4 pos : POSITION, float3 normal : NORMAL, float2 uv : TEXCOOR
 	VSOutput output; // ピクセルシェーダーに渡す値
 	output.svpos = mul(mat, pos);
 
-	output.color.rgb = dot(-lightdir, normal) * m_diffuse * lightcolor;
+	float3 ambient = m_ambient;
+
+	float3 diffuse = dot(-lightdir, normal) * m_diffuse;
+
+	const float3 eye = float3(0, 0, -20);
+	const float shininess = 4.0f;
+	float3 eyedir = normalize(eye - pos.xyz);
+	float3 reflect = normalize(lightdir + 2 * dot(-lightdir, normal) * normal);
+	float3 specular = pow(saturate(dot(reflect, eyedir)), shininess) * m_specular;
+
+
+	//output.color.rgb = dot(-lightdir, normal) * m_diffuse * lightcolor;
+	output.color.rgb = (ambient + diffuse + specular) * lightcolor;
 	output.color.a = m_alpha;
-//	output.normal = normal;
+	//	output.normal = normal;
 	output.uv = uv;
 	return output;
 }
