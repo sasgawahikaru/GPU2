7f20438120eec968a47aab17f7016babb817ddd7
sasagawahikaru <k021g1383@g.neec.ac.jp>
2022-12-09 10:43:50 +0900

    4_1　終わり

--- Resources/shaders/BasicPixelShader.hlsl
+++ Resources/shaders/BasicPixelShader.hlsl
@@ -10,5 +10,5 @@ float4 main(VSOutput input) : SV_TARGET
 	float brightness = diffuse + 0.3f;
 	float4 texcolor = tex.Sample(smp, input.uv) * color;
 	return float4(texcolor.rgb * brightness, texcolor.a);
-	//return float4(1, 1, 1, 1);
+//	return float4(1, 1, 1, 1);
 }
